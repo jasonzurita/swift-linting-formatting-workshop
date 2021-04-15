01aebcf927a45113b60b10346a0d0ef6a1f1b3aa
Jason Zurita <jasonzurita@me.com>
2021-04-15 14:15:29 -0400

    wip

---
+++ quotes/QuoteModel.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+/*
+ Below is from: https://github.com/lukePeavey/quotable
+ {
+    "length" : 76,
+    "_id" : "VDDodhULiss2",
+    "content" : "It is through science that we prove, but through intuition that we discover.",
+    "tags" : [
+       "famous-quotes",
+       "science",
+       "technology"
+    ],
+    "authorSlug" : "henri-poincare",
+    "author" : "Henri Poincaré"
+ }
+*/
+
+// swiftlint:disable identifier_name
+struct Quote: Codable {
+    let length: Int
+    let id, content: String
+    let tags: [String]
+    let authorSlug, author: String
+
+    enum CodingKeys: String, CodingKey {
+        case length
+        case id = "_id"
+        case content, tags, authorSlug, author
+    }
+}
+// swiftlint:enable identifier_name
