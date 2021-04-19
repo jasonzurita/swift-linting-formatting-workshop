811dddc3f0144486f56736770cc02ae685974cea
Jason Zurita <jasonzurita@me.com>
2021-04-18 21:26:10 -0400

    danger wip

---
+++ quotes/quotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  quotesApp.swift
+//  quotes
+//
+//  Created by Jason Zurita on 4/05/21.
+//
+
+import SwiftUI
+
+@main
+struct QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
